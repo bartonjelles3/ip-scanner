e1256d873cbb05ecf71e4100fcbd5f45dee60b40
bartonjelles3 <bartonjelles3@gmail.com>
2023-06-28 09:44:27 +0000

    Clean up comments

--- flask/api/query.py
+++ flask/api/query.py
@@ -1,9 +1,7 @@
 import requests
 
-# Define the URL of the Flask application
 url = 'http://127.0.0.1:5000/scan'
 
-# Prepare the request payload
 payload = {
     'ips': ['127.0.0.1:8080'],
     'scan_software': True,
@@ -12,12 +10,9 @@ payload = {
     'log_level': 'WARNING'
 }
 
-# Send the POST request
 response = requests.post(url, json=payload)
 
-# Check the response status code
 if response.status_code == 200:
-    # Print the response content
     print(response.json())
 else:
     print(f'Request failed with status code {response.status_code}')
